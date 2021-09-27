cf5f551aa130d33f0febcabc3fa0352b0d7a4fd9
Ammar Nanjiani <ammar123456@gmail.com>
2021-09-27 14:20:45 -0500

    autopep8 formatting

--- dbtools.py
+++ dbtools.py
@@ -3,39 +3,40 @@ import psycopg2
 import sqlite3
 import pandas as pd
 
+
 def pull_from_calibre(path):
     con = sqlite3.connect(path)
     tables = {
-        'author' : pd.read_sql_query("SELECT id, sort FROM authors", con),
-        'books' : pd.read_sql_query("SELECT id, title, pubdate FROM books", con),
-        'books_authors_link' : pd.read_sql_query("SELECT book, author FROM books_authors_link", con),
-        'sdr' : pd.read_sql_query("SELECT id, book, value FROM custom_column_6", con),
-        'books_tags_link' : pd.read_sql_query("SELECT book, tag FROM books_tags_link", con),
-        'tags' : pd.read_sql_query("SELECT id, name FROM tags", con)
+        'author': pd.read_sql_query("SELECT id, sort FROM authors", con),
+        'books': pd.read_sql_query("SELECT id, title, pubdate FROM books", con),
+        'books_authors_link': pd.read_sql_query("SELECT book, author FROM books_authors_link", con),
+        'sdr': pd.read_sql_query("SELECT id, book, value FROM custom_column_6", con),
+        'books_tags_link': pd.read_sql_query("SELECT book, tag FROM books_tags_link", con),
+        'tags': pd.read_sql_query("SELECT id, name FROM tags", con)
     }
     con.close()
 
     return tables
 
-def update_db(tables, con): 
-    # get the current values in the database for each table. 
+
+def update_db(tables, con):
+    # get the current values in the database for each table.
     cur_dfs = {
-    'books': pd.read_sql_query("SELECT * FROM books", con),
-    'annotations': pd.read_sql_query("SELECT * FROM annotations", con),
-    'books_tags_link': pd.read_sql_query("SELECT * FROM books_tags_link", con),
-    'tags': pd.read_sql_query("SELECT * FROM tags", con)
+        'books': pd.read_sql_query("SELECT * FROM books", con),
+        'annotations': pd.read_sql_query("SELECT * FROM annotations", con),
+        'books_tags_link': pd.read_sql_query("SELECT * FROM books_tags_link", con),
+        'tags': pd.read_sql_query("SELECT * FROM tags", con)
     }
 
     # add the values from calibre
     final_dfs = {
-        'books' : pd.concat([cur_dfs['books'].reset_index(drop = True), tables['books']]).drop_duplicates(keep=False),
-        'annotations' : pd.concat([cur_dfs['annotations'].reset_index(drop = True), tables['annotations']]).drop_duplicates(keep=False),
-        'books_tags_link' : pd.concat([cur_dfs['books_tags_link'].reset_index(drop = True), tables['books_tags_link']]).drop_duplicates(keep=False),
-        'tags' : pd.concat([cur_dfs['tags'].reset_index(drop = True), tables['tags']]).drop_duplicates(keep=False)
+        'books': pd.concat([cur_dfs['books'].reset_index(drop=True), tables['books']]).drop_duplicates(keep=False),
+        'annotations': pd.concat([cur_dfs['annotations'].reset_index(drop=True), tables['annotations']]).drop_duplicates(keep=False),
+        'books_tags_link': pd.concat([cur_dfs['books_tags_link'].reset_index(drop=True), tables['books_tags_link']]).drop_duplicates(keep=False),
+        'tags': pd.concat([cur_dfs['tags'].reset_index(drop=True), tables['tags']]).drop_duplicates(keep=False)
     }
 
     # send dataframe to postgres table
     for k, v in final_dfs.items():
         print(k, v)
-        v.to_sql(k, con, if_exists = 'append', index = False)
-    
+        v.to_sql(k, con, if_exists='append', index=False)
--- parse.py
+++ parse.py
@@ -7,31 +7,37 @@ import re
 # create dataframe of tags with private keys to relate to annotations.
 # annotations should also have private keys.
 # just replace entire database interaction code with django rest api.
+
+
 def parse_note(highlight, note):
-    
+
     if highlight in note:
         note = re.sub("Page [0-9]+ ", '', note)
-        note = re.sub(" @ [0-9]+[-][0-9]+[-][0-9]+ [0-9]+[:][0-9]+[:][0-9]+", '', note)
+        note = re.sub(
+            " @ [0-9]+[-][0-9]+[-][0-9]+ [0-9]+[:][0-9]+[:][0-9]+", '', note)
         note = note.replace(highlight, '')
-    
+
     return note
 
+
 def parse_sidecar(data, book):
-    bookmarks = pd.DataFrame(columns = ['datetime', 'chapter', 'text', 'pos0', 'pos1'])
-    highlights = pd.DataFrame(columns = ['datetime', 'chapter', 'text', 'pos0', 'pos1'])
+    bookmarks = pd.DataFrame(
+        columns=['datetime', 'chapter', 'text', 'pos0', 'pos1'])
+    highlights = pd.DataFrame(
+        columns=['datetime', 'chapter', 'text', 'pos0', 'pos1'])
 
     # parse bookmarks from json provided by calibre database
     for i in data['bookmarks'].values():
         if 'text' in i.keys():
             bookmarks = bookmarks.append(
                 {
-                    'datetime' : i['datetime'],
+                    'datetime': i['datetime'],
                     'chapter': i['chapter'],
                     'text': i['text'],
                     'pos0': i['pos0'],
                     'pos1': i['pos1']
                 },
-                ignore_index = True
+                ignore_index=True
             )
 
     # as above but with highlights
@@ -39,27 +45,31 @@ def parse_sidecar(data, book):
         for j in i.values():
             highlights = highlights.append(
                 {
-                    'datetime' : j['datetime'],
-                    'chapter' : j['chapter'],
-                    'text' : j['text'],
-                    'pos0' : j['pos0'],
-                    'pos1' : j['pos1'],
+                    'datetime': j['datetime'],
+                    'chapter': j['chapter'],
+                    'text': j['text'],
+                    'pos0': j['pos0'],
+                    'pos1': j['pos1'],
                 },
-                ignore_index = True
+                ignore_index=True
             )
 
     # combine highlights and bookmarks, redefine as "annotations"
-    a = highlights.merge(bookmarks, on=['datetime', 'chapter', 'pos0', 'pos1'], how='outer').rename(columns={'text_x': 'highlight', 'text_y': 'note', 'pos0': 'location'})[['highlight', 'note', 'location', 'chapter', 'datetime']]
-    
-    a.loc[pd.notna(a['note']), 'note'] = [parse_note(h, n) for h, n in a[pd.notna(a['note'])][['highlight', 'note']].itertuples(index=False)]
+    a = highlights.merge(bookmarks, on=['datetime', 'chapter', 'pos0', 'pos1'], how='outer').rename(columns={
+        'text_x': 'highlight', 'text_y': 'note', 'pos0': 'location'})[['highlight', 'note', 'location', 'chapter', 'datetime']]
+
+    a.loc[pd.notna(a['note']), 'note'] = [parse_note(h, n) for h, n in a[pd.notna(
+        a['note'])][['highlight', 'note']].itertuples(index=False)]
     print(a['note'])
     a['book'] = book
 
     return a
 
+
 def parse_all_sidecars(df):
     # define columns for annotations df
-    annotations = pd.DataFrame(columns = ['book', 'highlight', 'note', 'location', 'chapter', 'datetime'])
+    annotations = pd.DataFrame(
+        columns=['book', 'highlight', 'note', 'location', 'chapter', 'datetime'])
 
     for index, row in df.iterrows():
         val = row['value']
@@ -68,7 +78,6 @@ def parse_all_sidecars(df):
         a = parse_sidecar(data, row['book'])
 
         # store all the annotations for current book
-        annotations = pd.concat([annotations, a]).reset_index(drop = True)
+        annotations = pd.concat([annotations, a]).reset_index(drop=True)
 
     return annotations
-
